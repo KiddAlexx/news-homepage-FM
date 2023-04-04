69c2b5138d4b215811d3738cc57bb5bda53f99da
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-04-04 17:24:03 +0200

    add animation to mobile menu

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@ const body = document.querySelector('body');
 const btnOpenMenu = document.querySelector('.menu-open-btn');
 const btnCloseMenu = document.querySelector('.menu-close-btn');
 
-const mobileMenu = document.querySelector('.mobile-menu-closed');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 const openMobileMenu = function () {
   btnOpenMenu.addEventListener('click', e => {
     mobileMenu.classList.toggle('mobile-menu-open');
+
+    /* check to ensure that class is only toggled if it is present first */
+    if (mobileMenu.classList.contains('mobile-menu-close')) {
+      mobileMenu.classList.toggle('mobile-menu-close');
+    }
     body.classList.toggle('lock-scroll');
   });
 };
@@ -17,6 +22,7 @@ const openMobileMenu = function () {
 const closeMobileMenu = function () {
   btnCloseMenu.addEventListener('click', e => {
     mobileMenu.classList.toggle('mobile-menu-open');
+    mobileMenu.classList.toggle('mobile-menu-close');
     body.classList.toggle('lock-scroll');
   });
 };
