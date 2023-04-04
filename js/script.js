a069967cbfb82e371db0483ec70f4dc4d707afbf
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-04-04 16:43:57 +0200

    add basic functions to open and close mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-"use strict";
+'use strict';
+
+const btnOpenMenu = document.querySelector('.menu-open-btn');
+const btnCloseMenu = document.querySelector('.menu-close-btn');
+
+const mobileMenu = document.querySelector('.mobile-menu-closed');
+
+const openMobileMenu = function () {
+  btnOpenMenu.addEventListener('click', e => {
+    mobileMenu.classList.toggle('mobile-menu-open');
+    btnCloseMenu.classList.toggle('hidden');
+    console.log(e);
+  });
+};
+const closeMobileMenu = function () {
+  btnCloseMenu.addEventListener('click', e => {
+    mobileMenu.classList.toggle('mobile-menu-open');
+    btnCloseMenu.classList.toggle('hidden');
+    console.log(e);
+  });
+};
+
+openMobileMenu();
+closeMobileMenu();
