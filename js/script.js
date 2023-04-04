cfc13faf7b0bd67b716a4a11fafeb94629aaea28
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-04-04 16:49:01 +0200

    adjust transition for mobile menu + change menu functions - fixing issue of dissapearing close button

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ const mobileMenu = document.querySelector('.mobile-menu-closed');
 const openMobileMenu = function () {
   btnOpenMenu.addEventListener('click', e => {
     mobileMenu.classList.toggle('mobile-menu-open');
-    btnCloseMenu.classList.toggle('hidden');
+
     console.log(e);
   });
 };
 const closeMobileMenu = function () {
   btnCloseMenu.addEventListener('click', e => {
     mobileMenu.classList.toggle('mobile-menu-open');
-    btnCloseMenu.classList.toggle('hidden');
+
     console.log(e);
   });
 };
